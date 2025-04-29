2a5614d4526a84155798e087a2081c1eaa46b985
Francis Omondi <francisomondi17@gmail.com>
2025-04-29 11:07:25 +0300

    auth controller boiler plate

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req,res)=>{
+    res.send("this is the signup route")
+}
+
+export const login = async (req,res)=>{
+    res.send("this is the login route")
+}
+export const logout = async (req,res)=>{
+    res.send("this is the logout route")
+}
+
