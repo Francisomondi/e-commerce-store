c729d1ebe95c86741841a2254a46a5786d5b17ab
Francis Omondi <francisomondi17@gmail.com>
2025-05-06 02:14:37 +0300

    featured products

--- backend/controllers/products.controller.js
+++ backend/controllers/products.controller.js
@@ -16,3 +16,24 @@ export const getSingleProduct = async (req,res)=>{
         res.status(500).json({message: error.message})
     }
 }
+
+export const getFeaturedProducts = async (req,res)=>{
+    try {
+        await redis.get("featured_products")
+        if (featured_products) {
+            return res.json(JSON.parse(featured_products))  
+        }
+
+        //if not in redis, fetch from mongo db
+        const featured_products = await Product.find({isfeatured: true}).lean()
+
+        if(!featured_products.length){
+            return res.status(404).json({message: "No featured products found"})
+        }
+        await redis.set("featured_products", JSON.stringify(featured_products))
+
+        res.status(200).json({message: "Products fetched successfully"}, featured_products)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
--- backend/routes/products.route.js
+++ backend/routes/products.route.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { getProducts, getSingleProduct } from "../controllers/products.controller.js"
+import { getProducts, getSingleProduct,getFeaturedProducts } from "../controllers/products.controller.js"
 import { adminRoute, protectedRoute } from "../middleware/auth.middleware.js"
 
 const router = express.Router()
 
 router.get("/", protectedRoute, adminRoute, getProducts)
+router.get("/featured", getFeaturedProducts)
 router.get("/:id", getSingleProduct)
 
 export default router
