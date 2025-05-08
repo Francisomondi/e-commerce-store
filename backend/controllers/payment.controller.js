1bfa13ddc106fcba9466d4046f483c78d6494029
Francis Omondi <francisomondi17@gmail.com>
2025-05-08 06:55:53 +0300

    stripe checkout session, payment coupon controller function

--- backend/controllers/payment.controller.js
+++ backend/controllers/payment.controller.js
@@ -34,16 +34,43 @@ export const createCheckoutSession = async (req, res) => {
            payment_method_types: ["card"],
            line_items: lineItems,
            mode: "payment",
-           success_url: `${process.env.CLIENT_URL}/success`,
+           success_url: `${process.env.CLIENT_URL}/success/session_id={CHECKOUT_SESSION_ID}`,
            cancel_url: `${process.env.CLIENT_URL}/cancel`,
+           discounts: coupon ? [{coupon: await createStripeCoupon(coupon.discount)}] : [],
            metadata: {
-               userId: req.user._id,
-               couponId: coupon?._id
+               userId: req.user._id.toString,
+               couponId: coupon?._id,
+               couponCode:couponCode || ""
            }
        })
-       res.json({ url: session.url })
+       if(totalAmount >= 200000){
+           await createNewCoupon(req.user._id)
+           res.status(200).json({id: session.id,totalAmount: totalAmount})
+       }
        
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
+}
+
+async function createStripeCoupon(discount) {
+    const stripeCoupon = await stripe.coupons.create({
+        duration: "once",
+        percent_off: discount
+    })
+    return stripeCoupon.id
+}
+
+async function createNewCoupon(userId, code, discount, expiryDate) {
+    const newCoupon = new couponModel({
+        
+        code: "GIFT" + Math.random().toString(36).substring(2,8).toUpperCase(),
+        discount:10,
+        expiryDate: new Date(Date.now() + 30 + 24 * 60 * 60 * 1000),
+        userId: userId
+    })
+
+    await newCoupon.save()
+    return newCoupon
+
 }
