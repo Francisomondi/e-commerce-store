93c3eb47ba93d7023da2831a36c7bb6f0a5c553a
Francis Omondi <francisomondi17@gmail.com>
2025-05-09 11:49:27 +0300

    tailwind css config

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='min-h-screen text-white relative overflow-hidden'>
+      <h1 className=''> Home page - this is the home page</h1>
+    </div>
+  )
+}
+
+export default Home
