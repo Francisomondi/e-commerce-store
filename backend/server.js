9189134ff2cd7f4623a7c27d3b23545e738139b4
Francis Omondi <francisomondi17@gmail.com>
2025-05-13 10:30:05 +0300

    all products ui and fetching all peoducts

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(cors({
   credentials: true // If you're using cookies or auth headers
 }));
 
-app.use(express.json())
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
--- frontend/src/stores/useProductStore.js
+++ frontend/src/stores/useProductStore.js
@@ -19,11 +19,29 @@ export const useProductStore = create((set) => ({
         }
     },
 
-    getProducts: async () => {
+    fetchAllProducts: async () => {
         set({ loading: true });
         try {
             const res = await axios.get("/products");
-            set({ products: res.data, loading: false });
+            set({ products: res.data.products, loading: false });
+        } catch (error) {
+            set({ loading: false });
+            toast.error(error.response.data.message || "An error occurred");
+        }
+    },
+     deleteProduct: async () => {
+        set({ loading: true });
+        try {
+         
+        } catch (error) {
+            set({ loading: false });
+            toast.error(error.response.data.message || "An error occurred");
+        }
+    },
+     toggleFeaturedProduct: async () => {
+        set({ loading: true });
+        try {
+          
         } catch (error) {
             set({ loading: false });
             toast.error(error.response.data.message || "An error occurred");
